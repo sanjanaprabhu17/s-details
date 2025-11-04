6f902032b41c82eea14793cd8d811e61476d5a13
sanjanaprabhu17 <sanjanaprabhu617@gmail.com>
2025-11-04 11:51:27 +0530

    Update s.py

--- s.py
+++ s.py
@@ -3,7 +3,7 @@ if len(sys.argv) == 3:
      script_name = sys.argv[0]
      name = sys.argv[1]
      rollno = sys.argv[2]
-    print("User provided input values:")
+print("User provided input values:")
 else:
    script_name = sys.argv[0]
    name = "Sanjana"
