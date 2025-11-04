e0de6353cd1b18ab72c3e2db39643677617fb31f
sanjanaprabhu17 <sanjanaprabhu617@gmail.com>
2025-11-04 11:54:29 +0530

    Update s.py

--- s.py
+++ s.py
@@ -1,15 +1,16 @@
 import sys
-if len(sys.argv) == 3:
-     script_name = sys.argv[0]
-     name = sys.argv[1]
-     rollno = sys.argv[2]
-print("User provided input values:")
-else:
-   script_name = sys.argv[0]
-   name = "Sanjana"
-   rollno = "52"
-    print("no input given using default values:")
 
-print("script name:",script_name)
-print("student name:",name)
-print("roll number:",rollno)
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("User provided input values:")
+else:
+    script_name = sys.argv[0]
+    name = "misbah"
+    rollno = "058"
+    print("No input given, using default values:")
+
+print("Script name:", script_name)
+print("Student name:", name)
+print("Roll number:", rollno)
